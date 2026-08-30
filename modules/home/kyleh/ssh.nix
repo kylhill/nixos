e89314d8a5bdf77caeb36ed5bf06db32cc068449
Kyle Hill <kylhill@gmail.com>
2026-08-30 11:28:08 -0500

    Fix ssh.nix

--- modules/home/kyleh/ssh.nix
+++ modules/home/kyleh/ssh.nix
@@ -5,6 +5,12 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
+
+    # Home Manager 26.05 requires the default Host * block to be declared
+    # whenever raw trailing configuration is supplied through extraConfig.
+    # The actual defaults remain in files/ssh.conf so their OpenSSH ordering is
+    # preserved during the initial dotfiles migration.
+    settings."*" = { };
     extraConfig = builtins.readFile ./files/ssh.conf;
   };
 
