9d87425c40d566c0c22ea451a78c48c9eb0c4d8c
Kyle Hill <kylhill@gmail.com>
2026-09-02 13:52:51 -0500

    Update neovim config to more closely replicate lazyvim

--- modules/home/kyleh/neovim.nix
+++ modules/home/kyleh/neovim.nix
@@ -1,7 +1,15 @@
 {
+  config,
   pkgs,
   ...
 }:
+let
+  mkSnacksMap = key: command: desc: {
+    inherit key;
+    action = "<cmd>lua ${command}<cr>";
+    options = { inherit desc; };
+  };
+in
 {
   programs.nixvim = {
     enable = true;
@@ -20,28 +28,67 @@
 
     opts = {
       autoindent = true;
+      autowrite = true;
+      clipboard.__raw = ''
+        vim.env.SSH_CONNECTION and "" or "unnamedplus"
+      '';
+      confirm = true;
       cursorline = true;
       expandtab = true;
       hidden = true;
       ignorecase = true;
+      inccommand = "nosplit";
+      laststatus = 3;
       list = false;
       modeline = false;
+      mouse = "a";
       number = true;
       relativenumber = false;
-      scrolloff = 3;
+      scrolloff = 4;
       shiftwidth = 4;
       showmatch = true;
       sidescroll = 1;
-      sidescrolloff = 2;
+      sidescrolloff = 8;
+      signcolumn = "yes";
       smartcase = true;
       smarttab = true;
+      smoothscroll = true;
       softtabstop = -1;
       splitbelow = true;
       splitright = true;
       tabstop = 4;
       termguicolors = true;
+      timeoutlen = 300;
+      undofile = true;
+      undolevels = 10000;
+      updatetime = 200;
       wrap = false;
     };
+
+    keymaps = [
+      (mkSnacksMap "<leader><space>" "Snacks.picker.files()" "Find Files")
+      (mkSnacksMap "<leader>," "Snacks.picker.buffers()" "Buffers")
+      (mkSnacksMap "<leader>/" "Snacks.picker.grep()" "Grep")
+      (mkSnacksMap "<leader>:" "Snacks.picker.command_history()" "Command History")
+      (mkSnacksMap "<leader>e" "Snacks.explorer()" "Explorer")
+      (mkSnacksMap "<leader>fb" "Snacks.picker.buffers()" "Buffers")
+      (mkSnacksMap "<leader>ff" "Snacks.picker.files()" "Find Files")
+      (mkSnacksMap "<leader>fr" "Snacks.picker.recent()" "Recent Files")
+      (mkSnacksMap "<leader>sb" "Snacks.picker.lines()" "Buffer Lines")
+      (mkSnacksMap "<leader>sg" "Snacks.picker.grep()" "Grep")
+      (
+        (mkSnacksMap "<leader>sw" "Snacks.picker.grep_word()" "Visual Selection or Word")
+        // {
+          mode = [
+            "n"
+            "x"
+          ];
+        }
+      )
+      (mkSnacksMap "<leader>sh" "Snacks.picker.help()" "Help Pages")
+      (mkSnacksMap "<leader>sk" "Snacks.picker.keymaps()" "Keymaps")
+      (mkSnacksMap "<leader>sR" "Snacks.picker.resume()" "Resume")
+    ];
 
     plugins = {
       blink-cmp = {
@@ -51,12 +98,50 @@
       bufferline.enable = true;
       conform-nvim.enable = true;
       gitsigns.enable = true;
-      lualine.enable = true;
-      noice.enable = true;
-      telescope.enable = true;
+      lualine = {
+        enable = true;
+        settings.options.globalstatus = true;
+      };
+      mini-ai.enable = true;
+      mini-pairs.enable = true;
+      noice = {
+        enable = true;
+        settings.presets = {
+          bottom_search = true;
+          command_palette = true;
+          long_message_to_split = true;
+        };
+      };
+      snacks = {
+        enable = true;
+        settings = {
+          explorer.enabled = true;
+          picker.enabled = true;
+        };
+      };
       todo-comments.enable = true;
-      treesitter.enable = true;
+      treesitter = {
+        enable = true;
+        highlight.enable = true;
+        grammarPackages =
+          with config.programs.nixvim.plugins.treesitter.package.builtGrammars;
+          [
+            bash
+            diff
+            json
+            lua
+            markdown
+            markdown_inline
+            nix
+            query
+            regex
+            vim
+            vimdoc
+            yaml
+          ];
+      };
       trouble.enable = true;
+      ts-comments.enable = true;
       web-devicons.enable = true;
       which-key.enable = true;
 
@@ -81,6 +166,5 @@
       pkgs.nixfmt
       pkgs.stylua
     ];
-
   };
 }
