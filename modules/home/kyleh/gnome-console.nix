6a1cd6c181a0742fac9047347173c47cd08e5f3a
Kyle Hill <kylhill@gmail.com>
2026-09-04 09:51:33 -0500

    Fix formatting

--- modules/home/kyleh/gnome-console.nix
+++ modules/home/kyleh/gnome-console.nix
@@ -9,29 +9,89 @@ let
     ;
 
   colors = {
-    base00 = [ 0 43 54 ];
-    base01 = [ 7 54 66 ];
-    base02 = [ 88 110 117 ];
-    base03 = [ 101 123 131 ];
-    base04 = [ 131 148 150 ];
-    base05 = [ 147 161 161 ];
-    base06 = [ 238 232 213 ];
-    base07 = [ 253 246 227 ];
-    base08 = [ 220 50 47 ];
-    base09 = [ 203 75 22 ];
-    base0A = [ 181 137 0 ];
-    base0B = [ 133 153 0 ];
-    base0C = [ 42 161 152 ];
-    base0D = [ 38 139 210 ];
-    base0E = [ 108 113 196 ];
-    base0F = [ 211 54 130 ];
+    base00 = [
+      0
+      43
+      54
+    ];
+    base01 = [
+      7
+      54
+      66
+    ];
+    base02 = [
+      88
+      110
+      117
+    ];
+    base03 = [
+      101
+      123
+      131
+    ];
+    base04 = [
+      131
+      148
+      150
+    ];
+    base05 = [
+      147
+      161
+      161
+    ];
+    base06 = [
+      238
+      232
+      213
+    ];
+    base07 = [
+      253
+      246
+      227
+    ];
+    base08 = [
+      220
+      50
+      47
+    ];
+    base09 = [
+      203
+      75
+      22
+    ];
+    base0A = [
+      181
+      137
+      0
+    ];
+    base0B = [
+      133
+      153
+      0
+    ];
+    base0C = [
+      42
+      161
+      152
+    ];
+    base0D = [
+      38
+      139
+      210
+    ];
+    base0E = [
+      108
+      113
+      196
+    ];
+    base0F = [
+      211
+      54
+      130
+    ];
   };
   solarizedDarkUuid = "043f5921-e1fb-4f08-91b1-6f9e936b85a7";
-  rgb =
-    name:
-    mkTuple (
-      map (value: mkDouble (value / 255.0)) colors.${name}
-    );
+  rgb = name: mkTuple (map (value: mkDouble (value / 255.0)) colors.${name});
   dictionary =
     entries:
     mkArray "{sv}" (
